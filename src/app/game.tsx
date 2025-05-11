766105c0a15541736e6f890836e3555eb2d7e1b7
pererikljung <pererikljung@gmail.com>
2025-05-11 18:46:45 +0200

    Working Tic-Tac-Toe

---
+++ src/app/game.tsx
@@ -0,0 +1,75 @@
+'use client'
+import {useState} from "react";
+
+function Button({value, buttonClick}) {
+  return (
+    <button onClick={buttonClick} className="outline-solid h-(--buttonsize) w-(--buttonsize) text-(length:--buttontextsize)" >{value}</button>
+  )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)){
+            return;
+          }
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = "X";
+          } else {
+            nextSquares[i] = "O";
+          }
+        setXIsNext(!xIsNext);
+        setSquares(nextSquares);
+      }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+  return (
+    
+    <div className="grid grid-cols-3 gap-8">
+        <div className="col-span-3 text-center">{status}</div>
+        < Button value={squares[0]} buttonClick={() => handleClick(0)}/>
+        < Button value={squares[1]} buttonClick={() => handleClick(1)}/>
+        < Button value={squares[2]} buttonClick={() => handleClick(2)}/>
+
+        < Button value={squares[3]} buttonClick={() => handleClick(3)}/>
+        < Button value={squares[4]} buttonClick={() => handleClick(4)}/>
+        < Button value={squares[5]} buttonClick={() => handleClick(5)}/>
+
+        < Button value={squares[6]} buttonClick={() => handleClick(6)}/>
+        < Button value={squares[7]} buttonClick={() => handleClick(7)}/>
+        < Button value={squares[8]} buttonClick={() => handleClick(8)}/>
+      </div>
+    
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+'use client'
 import Image from "next/image";
+import React from "react";
+import Game from "./game";
 
 export default function Home() {
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <div className="place-items-center">
+        <h1 className="text-xl">Welcome to Tre-I-Rad!</h1>
+        <p className="text-sm">Tre-I-Rad is the swedish equivalent for tic-tac-toe. Made by Per-Erik Ljung with only limited tutorial knowledge from the past and only documentations from the technologi that was used. </p>
+      </div>
+      <main>
+        < Game/>
       </main>
       <footer>
-
       </footer>
     </div>
   );
